5dcd52f633fe6ad6f330ed19175825b4584705a6
wenxin0319 <cwx0319@gmail.com>
2022-11-23 00:07:09 -0800

    add

--- chair.js
+++ chair.js
@@ -23,6 +23,100 @@ class Cube extends Shape {
     }
 }
 
+export class Shape_From_File extends Shape
+{                                   // **Shape_From_File** is a versatile standalone Shape that imports
+                                    // all its arrays' data from an .obj 3D model file.
+    constructor( filename )
+    { super( "position", "normal", "texture_coord" );
+        // Begin downloading the mesh. Once that completes, return
+        // control to our parse_into_mesh function.
+        this.load_file( filename );
+    }
+    load_file( filename )
+    {                             // Request the external file and wait for it to load.
+        // Failure mode:  Loads an empty shape.
+        return fetch( filename )
+            .then( response =>
+            { if ( response.ok )  return Promise.resolve( response.text() )
+            else                return Promise.reject ( response.status )
+            })
+            .then( obj_file_contents => this.parse_into_mesh( obj_file_contents ) )
+            .catch( error => { this.copy_onto_graphics_card( this.gl ); } )
+    }
+    parse_into_mesh( data )
+    {                           // Adapted from the "webgl-obj-loader.js" library found online:
+        var verts = [], vertNormals = [], textures = [], unpacked = {};
+
+        unpacked.verts = [];        unpacked.norms = [];    unpacked.textures = [];
+        unpacked.hashindices = {};  unpacked.indices = [];  unpacked.index = 0;
+
+        var lines = data.split('\n');
+
+        var VERTEX_RE = /^v\s/;    var NORMAL_RE = /^vn\s/;    var TEXTURE_RE = /^vt\s/;
+        var FACE_RE = /^f\s/;      var WHITESPACE_RE = /\s+/;
+
+        for (var i = 0; i < lines.length; i++) {
+            var line = lines[i].trim();
+            var elements = line.split(WHITESPACE_RE);
+            elements.shift();
+
+            if      (VERTEX_RE.test(line))   verts.push.apply(verts, elements);
+            else if (NORMAL_RE.test(line))   vertNormals.push.apply(vertNormals, elements);
+            else if (TEXTURE_RE.test(line))  textures.push.apply(textures, elements);
+            else if (FACE_RE.test(line)) {
+                var quad = false;
+                for (var j = 0, eleLen = elements.length; j < eleLen; j++)
+                {
+                    if(j === 3 && !quad) {  j = 2;  quad = true;  }
+                    if(elements[j] in unpacked.hashindices)
+                        unpacked.indices.push(unpacked.hashindices[elements[j]]);
+                    else
+                    {
+                        var vertex = elements[ j ].split( '/' );
+
+                        unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 0]);
+                        unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 1]);
+                        unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 2]);
+
+                        if (textures.length)
+                        {   unpacked.textures.push(+textures[( (vertex[1] - 1)||vertex[0]) * 2 + 0]);
+                            unpacked.textures.push(+textures[( (vertex[1] - 1)||vertex[0]) * 2 + 1]);  }
+
+                        unpacked.norms.push(+vertNormals[( (vertex[2] - 1)||vertex[0]) * 3 + 0]);
+                        unpacked.norms.push(+vertNormals[( (vertex[2] - 1)||vertex[0]) * 3 + 1]);
+                        unpacked.norms.push(+vertNormals[( (vertex[2] - 1)||vertex[0]) * 3 + 2]);
+
+                        unpacked.hashindices[elements[j]] = unpacked.index;
+                        unpacked.indices.push(unpacked.index);
+                        unpacked.index += 1;
+                    }
+                    if(j === 3 && quad)   unpacked.indices.push( unpacked.hashindices[elements[0]]);
+                }
+            }
+        }
+        {
+            const { verts, norms, textures } = unpacked;
+            for( var j = 0; j < verts.length/3; j++ )
+            {
+                this.arrays.position     .push( vec3( verts[ 3*j ], verts[ 3*j + 1 ], verts[ 3*j + 2 ] ) );
+                this.arrays.normal       .push( vec3( norms[ 3*j ], norms[ 3*j + 1 ], norms[ 3*j + 2 ] ) );
+                this.arrays.texture_coord.push( vec( textures[ 2*j ], textures[ 2*j + 1 ] ) );
+            }
+            this.indices = unpacked.indices;
+        }
+        this.normalize_positions( false );
+        this.ready = true;
+    }
+    draw( context, program_state, model_transform, material )
+    {               // draw(): Same as always for shapes, but cancel all
+        // attempts to draw the shape before it loads:
+        if( this.ready )
+            super.draw( context, program_state, model_transform, material );
+    }
+}
+
+
+
 class Base_Scene extends Scene {
     /**
      *  **Base_scene** is a Scene that can be added to any display canvas.
@@ -39,8 +133,13 @@ class Base_Scene extends Scene {
             'ball_1': new defs.Regular_2D_Polygon(1, 15),
             'ball_2': new defs.Subdivision_Sphere(4),
             'ball_3': new (defs.Subdivision_Sphere.prototype.make_flat_shaded_version())(1),
+            'alien': new Shape_From_File("assets/alien.obj"),
+            'capsul': new Shape_From_File("assets/capsul.obj"),
+            'robot': new Shape_From_File("assets/robot.obj"),
+            'spacecar': new Shape_From_File("assets/spacecar.obj"),
         };
         // *** Materials
+        const textured = new defs.Textured_Phong(1);
         this.materials = {
             wood: new Material(new defs.Phong_Shader(),
                 {ambient: .8, diffusivity: .6, color: hex_color("#BA8C63")}),
@@ -58,7 +157,29 @@ class Base_Scene extends Scene {
                 color: hex_color("#6B8E23"),
                 ambient: 1, diffusivity: 0.1,
                 texture: new Texture("assets/loc_05.png", "NEAREST")
-            })};
+            }),
+            alien_texture: new Material(textured, {
+                ambient: 0.1, diffusivity: 0.5,
+                texture: new Texture("assets/alien.png"),
+                color: color(0, 0, 0, 1)
+            }),
+            capsul_texture: new Material(textured, {
+                ambient: 0.1, diffusivity: 0.5,
+                texture: new Texture("assets/capsul.png"),
+                color: color(0, 0, 0, 1)
+            }),
+           robot_texture: new Material(textured, {
+                ambient: 0.1, diffusivity: 0.5,
+                texture: new Texture("assets/robot.png"),
+                color: color(0, 0, 0, 1)
+            }),
+            spacecar_texture: new Material(textured, {
+                ambient: 0.1, diffusivity: 0.5,
+                texture: new Texture("assets/spacecar.png"),
+                color: color(0, 0, 0, 1)
+            })
+
+        };
     }
 
     display(context, program_state) {
@@ -122,6 +243,25 @@ export class chair_scene extends Base_Scene {
         let back_transform = chair_model_transform.times(Mat4.translation(0, -4.1, 0)).times(Mat4.translation(0,2,-1))
             .times(Mat4.scale(1.2,1.2,0.25));
 
+        //draw alien
+        let alien_transform = Mat4.identity();
+        alien_transform = alien_transform.times(Mat4.translation(8, -3, -5));
+        this.shapes.alien.draw(context, program_state, alien_transform, this.materials.alien_texture);
+
+        //draw alien
+        let capsul_transform = Mat4.identity();
+        capsul_transform = capsul_transform.times(Mat4.translation(8, -7, -5));
+        this.shapes.capsul.draw(context, program_state, capsul_transform, this.materials.capsul_texture);
+
+        //draw alien
+        let robot_transform = Mat4.identity();
+        robot_transform = robot_transform.times(Mat4.translation(6, -3, -5));
+        this.shapes.robot.draw(context, program_state, robot_transform, this.materials.robot_texture);
+
+        //draw alien
+        let spacecar_transform = Mat4.identity();
+        spacecar_transform = spacecar_transform.times(Mat4.translation(4, -3, -5));
+        this.shapes.spacecar.draw(context, program_state, spacecar_transform, this.materials.spacecar_texture);
 
         this.shapes.cube.draw(context, program_state, leg_transform_1, this.materials.wood);
         this.shapes.cube.draw(context, program_state, leg_transform_2, this.materials.wood);
